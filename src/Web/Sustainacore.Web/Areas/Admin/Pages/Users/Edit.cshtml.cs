94d101e7924ea9e5814c4b3eb8a60e70bbf32e12
Your Name <you@example.com>
2025-10-17 02:41:29 +0200

    Add basic user management with in-memory users and role editing

---
+++ src/Web/Sustainacore.Web/Areas/Admin/Pages/Users/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sustainacore.Web.Areas.Admin.Pages.Users
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
